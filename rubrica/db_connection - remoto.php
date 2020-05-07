46e79e06b8c9ecea5bf7fc463a9551657c5d81f1
sviluppowebapp <64779093+sviluppowebapp@users.noreply.github.com>
2020-05-07 15:24:58 +0200

    Remove duplicate directory

--- rubrica/db_connection - remoto.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli('host', 'user', 'pwd', 'nome_db');
-		if ($mysqli->connect_error) {
-    		die('Errore di connessione (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-		} else {
-		}
-?>
--- rubrica/db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli('127.0.0.1', 'root', 'admin', 'iykqltua_rubrica');
-		if ($mysqli->connect_error) {
-    		die('Errore di connessione (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-		} else {
-		}
-?>
